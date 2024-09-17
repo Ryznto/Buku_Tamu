2b67ef7369941fd19e913440b510b5817854ec41
Ryznto <riyanto112211@gmail.com>
2024-09-17 08:32:40 +0700

    new file

---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php
+define('HOST_NAME', 'localhost');
+// define('HOST_NAME', '127.0.0.1');
+define('USER_NAME', 'root');
+define('PASSWORD', '');
+define('DB_NAME', 'app_bukutamu');
+
+$koneksi = mysqli_connect(HOST_NAME,USER_NAME,PASSWORD,DB_NAME);
+
+?>
+
+
+
